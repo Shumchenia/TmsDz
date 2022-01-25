e52fea445ec872ee7251095877670cb136c5b2d1
Matvei Shumchenia <shumchenia.mat@gmail.com>
2022-01-25 20:46:36 +0300

    init project

---
+++ main/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer a=5;
+        System.out.println(a>>>1);
+        System.out.println(a>>1);
+        System.out.println(a<< 0b1);
+        System.out.println(a&4);
+        System.out.println(a|4);
+        System.out.println(a^4);
+    }
+}
+/*
+* 1 1
+* 2 10
+* 3 11
+* 4 100
+* 5 101
+* 6 110
+* 7 111
+* 8 1000
+* 9 1001
+* 10 1010
+*
+*
+*
+*
+* */
